06a92d17ba5d0abbdc7560cf9f27c220215f0560
Omid-Sargazi <om.sargazi@gmail.com>
2025-09-13 17:27:26 +0330

    Create a simple Tree

---
+++ DesignPatterns/Trees/BinaryNode.cs
@@ -0,0 +1,29 @@
+namespace DesignPatterns.Trees
+{
+    public class BinaryNode
+    {
+        public int Value {get; set;}
+        public BinaryNode Left {get; set;}
+        public BinaryNode Right {get; set;}
+
+        public BinaryNode(int value)
+        {
+            Value = value;
+        }
+
+
+        public class RunBinaryNode
+        {
+            public static void Run()
+            {
+                var root = new BinaryNode(1);
+                root.Left = new BinaryNode(2);
+                root.Right = new BinaryNode(3);
+                root.Left.Left = new BinaryNode(4);
+                root.Left.Right = new BinaryNode(5);
+                root.Right.Left = new BinaryNode(6);
+
+            }
+        }
+    }
+}
