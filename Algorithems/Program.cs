aa95fc0e444c5e80452e8a2d23c545ad161eaa0e
Omid-Sargazi <om.sargazi@gmail.com>
2025-09-14 20:43:42 +0330

    Bubble Sort in C#

--- Algorithems/Program.cs
+++ Algorithems/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Algorithems.Sorting;
+
 Console.WriteLine("Hello, World!");
+var arr = new int[] { 1, 2, 37, 8, 70, -70, -90, 12, 0, 0 };
+Sortings.Bubble(arr);
---
+++ Algorithems/Sorting/Sortings.cs
@@ -0,0 +1,25 @@
+namespace Algorithems.Sorting
+{
+    public class Sortings
+    {
+        public static void Bubble(int[] arr)
+        {
+            for (int start = arr.Length - 1; start >= 0; start--)
+            {
+                bool swapped = false;
+                for (int j = 0; j < start; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        (arr[j], arr[j + 1]) = (arr[j + 1], arr[j]);
+                        swapped = true;
+                    }
+                }
+                if (!swapped) break;
+            }
+
+            Console.WriteLine($"{string.Join(",", arr)}");
+        }
+        
+    }
+}
