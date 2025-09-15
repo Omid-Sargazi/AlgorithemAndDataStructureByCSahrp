696bbb580193c499edd7fe13837be99353da0487
Omid-Sargazi <om.sargazi@gmail.com>
2025-09-15 19:09:12 +0330

    Select Many In Linq

--- DataStructures/LINQExamples/Example.cs
+++ DataStructures/LINQExamples/Example.cs
@@ -4,6 +4,12 @@ namespace DataStructures.LINQExamples
     {
         public static void Run()
         {
+
+            WhereExample();
+        }
+
+        private static void WhereExample()
+        {
             var numbers = new[] { 5, 3, 8, 1, 4, 7, 2, 6, 7, 8, 9, 10, };
             var numbesGreaterThanfive = numbers.Where(n => n > 5);
             Console.WriteLine($"Numbers greater than 5: {string.Join(",", numbesGreaterThanfive)}");
@@ -27,6 +33,20 @@ namespace DataStructures.LINQExamples
             var result = numbers.Where(n => n > 5).Where(n => n % 2 == 0);
             Console.WriteLine($"Numbers greater than 5 and even: {string.Join(",", result)}");
 
+            var students = new List<Student>
+            {
+                new Student { Name = "Omid", Courses = new List<string> { "Math", "Physics" } },
+                new Student { Name = "Saeed", Courses = new List<string> { "Biology", "Chemistry" } },
+                new Student { Name = "Vahid", Courses = new List<string> { "Math", "Biology" } },
+                new Student { Name = "Ali", Courses = new List<string> { "History", "Geography" } },
+            };
+
+            var query = students.Select(s => s.Courses);
+
+            foreach (var course in query)
+            {
+                Console.WriteLine($"Courses: {string.Join(", ", course)}");
+            }
         }
     }
 
@@ -34,5 +54,12 @@ namespace DataStructures.LINQExamples
     {
         public string Name { get; set; }
         public int Age { get; set; }
+    }
+
+
+    public class Student
+    {
+        public string Name { get; set; }
+        public List<string> Courses { get; set; }
     }
 }
