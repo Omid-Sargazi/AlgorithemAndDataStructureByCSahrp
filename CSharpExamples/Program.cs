c588a9a0307683120c1e8dfbc46429f688ea2488
Omid-Sargazi <om.sargazi@gmail.com>
2025-09-15 23:24:06 +0330

    Find the Minimum in Rotated Sorted Array

--- CSharpExamples/Program.cs
+++ CSharpExamples/Program.cs
@@ -1,5 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using CSharpExamples.Example1;
+using CSharpExamples.LeetCode;
 
 Console.WriteLine("Hello, World!");
-await Cancelation.Run();
+// await Cancelation.Run();
+
+var nums = new int[] { 4, 5, 6, 7, 0, 1, 2 };
+Console.WriteLine(MinimumRotatedSortedArray.Run(nums));
