11e72c06fb983184dc610a3a6db478ec05155de3
Omid-Sargazi <om.sargazi@gmail.com>
2025-09-15 21:24:16 +0330

    Print numbers with cancle

--- CSharpExamples/Example1/Cancelation.cs
+++ CSharpExamples/Example1/Cancelation.cs
@@ -1,3 +1,5 @@
+using System.Runtime.InteropServices;
+
 namespace CSharpExamples.Example1
 {
     public class Cancelation
@@ -5,13 +7,37 @@ namespace CSharpExamples.Example1
         public async static Task Run()
         {
             using var cts = new CancellationTokenSource();
+            // cts.CancelAfter(TimeSpan.FromSeconds(3));
 
-            cts.Cancel();
-            var task = DoWorkAsync(cts.Token);
 
-            await Task.Delay(500);
+            // cts.Cancel();
+            // var task = DoWorkAsync(cts.Token);
 
-            await task;
+            // await Task.Delay(500);
+
+            // await task;
+
+            var nums = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var task = PrintNumbersAsync(nums, cts.Token);
+            Console.WriteLine("Press enter to cancel...");
+            _ = Task.Run(() =>
+            {
+                Console.ReadKey();
+                cts.Cancel();
+            });
+
+            try
+            {
+                await task;
+            }
+            catch (OperationCanceledException)
+            {
+                
+                Console.WriteLine("Task was cancelled.");
+            }
+
+
+
         }
 
 
@@ -25,5 +51,16 @@ namespace CSharpExamples.Example1
 
             Console.WriteLine("Calcelled");
         }
+
+        public static async Task PrintNumbersAsync(List<int> nums, CancellationToken ct)
+        {
+            foreach (var n in nums)
+            {
+                ct.ThrowIfCancellationRequested();
+                Console.WriteLine(n);
+                await Task.Delay(1000, ct);
+            }
+        }
+        
     }
 }
