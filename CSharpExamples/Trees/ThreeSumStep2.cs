847f77c81748ee6527c79f13b93878ece8fb6b0d
Omid-Sargazi <om.sargazi@gmail.com>
2025-09-17 21:28:17 +0330

    .

--- CSharpExamples/Trees/ThreeSumStep2.cs
+++ CSharpExamples/Trees/ThreeSumStep2.cs
@@ -10,8 +10,9 @@ namespace CSharpExamples.Trees
             Console.WriteLine("Sorted array:" + string.Join(",", nums));
 
             int n = nums.Length;
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < n-2; i++)
             {
+                if (i > 0 && nums[i] == nums[i - 1]) continue;
                 int left = i + 1;
                 int right = n - 1;
 
@@ -20,20 +21,22 @@ namespace CSharpExamples.Trees
                    int sum = nums[i] + nums[left] + nums[right];
                 Console.WriteLine($"Fix nums[{i}]={nums[i]},left={left},right={right}");
 
-                if (sum == 0)
-                {
-                    Console.WriteLine($"✅ Found triplet: [{nums[i]}, {nums[left]}, {nums[right]}]");
-                    left++;
-                    right--;
+                    if (sum == 0)
+                    {
+                        Console.WriteLine($"✅ Found triplet: [{nums[i]}, {nums[left]}, {nums[right]}]");
+                        left++;
+                        right--;
+                        while (left < right && nums[left] == nums[left - 1]) left++;
+                        while (left < right && nums[right] == nums[right + 1]) right--;
                 }
-                else if (sum < 0)
-                {
-                    left++;
-                }
-                else
-                {
-                    right--;
-                }
+                    else if (sum < 0)
+                    {
+                        left++;
+                    }
+                    else
+                    {
+                        right--;
+                    }
              }
             }
         }
