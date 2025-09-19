63918bb781b114470f988e07ae8e3cde0c4dcf15
Omid-Sargazi <om.sargazi@gmail.com>
2025-09-19 23:06:49 +0330

    Create Binary Tree

---
+++ Algorithems/Trees/BSTNode.cs
@@ -0,0 +1,27 @@
+namespace Algorithems.Trees
+{
+    public class BSTNode
+    {
+        public int Value;
+        public BSTNode Left;
+        public BSTNode Right;
+
+        public BSTNode(int value)
+        {
+            Value = value;
+        }
+
+        public BSTNode Insert(BSTNode root, int value)
+        {
+            if (root == null) return new BSTNode(value);
+
+            if (value < root.Value)
+                root.Left = Insert(root.Left, value);
+
+            else if (value > root.Left)
+                root.Right = Insert(root.Right, value);
+
+            return root;
+        }
+    }
+}
