2f30f5d0dc742da39212a1c88fb2474b2346c290
Omid-Sargazi <om.sargazi@gmail.com>
2025-09-21 17:16:42 +0330

    Bubble Sort in C#

---
+++ CSharpExamples/Examples2/SortingInCSharp.cs
@@ -0,0 +1,24 @@
+namespace CSharpExamples.Examples2
+{
+    public class SortingInCSharp
+    {
+        public static void BubbleSort(int[] arr)
+        {
+            for (int start = arr.Length - 1; start >= 0; start--)
+            {
+                bool swapped = false;
+                for (int j = 0; j < start; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        (arr[j], arr[j + 1]) = (arr[j + 1], arr[j]);
+                        swapped = true;
+                    }
+                }
+                if (!swapped) break;
+            }
+
+            Console.WriteLine($"{string.Join(",",arr)}");
+        }
+    }
+}
